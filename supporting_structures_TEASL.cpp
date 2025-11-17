505a2a094849a1ad9948cc5f1ebbbcd7cab2a8ad
Jared Newell <jarednewell@users.noreply.github.com>
2025-11-17 09:24:00 +0100

    supporting_structures_TEASL.cpp
    TEASL node data container

--- supporting_structures_TEASL.cpp
+++ supporting_structures_TEASL.cpp
@@ -4,7 +4,7 @@
 struct TEASL_Node {
     /* data container */
     int Version; // version of identifier
-    TEASL_Node* left_pointer;
-    Node* equal_node_pointer;
-    TEASL_Node* right_pointer;
+    TEASL_Node* left_pointer; // pointer to ternary tree node (left)
+    Node* equal_node_pointer; // pointer to EASL node (centre)
+    TEASL_Node* right_pointer; // pointer to ternary tree node (right)
 };
